eda9549592e3e408f775b4bd19951e984c83aed1
Louis Roche <louis.roche@icloud.com>
2025-05-15 19:05:41 +0100

    yo tout le monde c'est squeezie

--- src/networks.py
+++ src/networks.py
@@ -190,7 +190,7 @@ class ReplayBuffer:
         begin = self.ptr
         states = torch.from_numpy(np.stack(s_array, axis=0)).to(self.dvc)
         actions = torch.tensor(a_array).to(self.dvc)
-        rewards = r_array.clone().detach().float().to(self.dvc)
+        rewards = r_array.clone().detach().float().to(self.dvc).squeeze(1)
         next_states = torch.from_numpy(np.stack(s_next_array, axis=0)).to(self.dvc)
         dws = torch.tensor(dw_array).unsqueeze(-1).to(self.dvc)
 
