ff3e7cf5eee33c0b66ed2c876af88c6c2d64c07a
Louis Roche <louis.roche@icloud.com>
2025-05-19 11:35:48 +0100

    fixed typo

--- src/embedder.py
+++ src/embedder.py
@@ -135,7 +135,7 @@ class RewardModel:
         image_embeddings = self.embedding_model.get_image_embedding(images_np)
         distance = self.compute_distance(image_embeddings, goal_embedding)
         r = torch.exp(-distance)
-        print("Mean reward of batch: " + r.mean().item())
+        print("Mean reward of batch: " + str(r.mean().item()))
         return r
 
     def compute_distance(
